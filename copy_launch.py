ce7188242f1c9e712665a061306f14f813c2418d
yym68686 <yym68686@outlook.com>
2023-05-18 17:21:29 +0800

    2023年 05月 18日 星期四 17:21:29 CST

--- copy_launch.py
+++ copy_launch.py
@@ -10,17 +10,18 @@ for word in words:
         config = word.strip()
 print('Load config from ', config)
 
-f = open(config, 'r')
-save_path = f.readlines()[-1].split(' ')[1].strip()
-f.close()
-print('Save code to ', save_path)
+# f = open(config, 'r')
+# save_path = f.readlines()[-1].split(' ')[1].strip()
+# f.close()
+# print('Save code to ', save_path)
 
-try:
-    os.mkdir(save_path)
-except Exception:
-    pass
-shutil.copytree(os.getcwd(), os.path.join(save_path, 'code'))
+# try:
+#     os.mkdir(save_path)
+# except Exception:
+#     pass
+
+# shutil.copytree(os.getcwd(), os.path.join(save_path, 'code'))
 print('Start process')
-os.chdir(os.path.join(save_path, 'code'))
+# os.chdir(os.path.join(save_path, 'code'))
 os.system('sh launch.sh')
 print('Done')
